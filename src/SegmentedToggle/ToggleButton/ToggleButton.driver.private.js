256fdfba0f8628d7b1909bd6b8028b523c564466
mykas <mykolass@wix.com>
2019-01-23 16:16:08 +0200

    <ToggleButton/> - change method handling

--- src/SegmentedToggle/ToggleButton/ToggleButton.driver.private.js
+++ src/SegmentedToggle/ToggleButton/ToggleButton.driver.private.js
@@ -1,10 +1,8 @@
-import { Simulate } from 'react-dom/test-utils';
-
 export const toggleButtonPrivateDriverFactory = base => {
   return {
     exists: async () => await base.exists(),
     getToggleText: async () => await base.text(),
     prefixExists: async () => await base.$('[data-hook="prefix"]').exists(),
-    toggle: async () => await base.$('input').getNative(),
+    toggle: async () => await base.$('input').click(),
   };
 };
