cde807086a37fad7dc824a76e846e14e09ba813b
Nitay Rabinovich <nitayr@wix.com>
2019-01-28 16:02:45 +0200

    Fix PR (Correct live code examples)

--- stories/CounterBadge/CounterBadge.story.js
+++ stories/CounterBadge/CounterBadge.story.js
@@ -1,13 +1,59 @@
 import React from 'react';
 
-import CodeExample from 'wix-storybook-utils/dist/src/CodeExample';
 import { SKIN } from 'wix-ui-backoffice/dist/src/components/StylableCounterBadge/constants';
 import { Email } from 'wix-ui-icons-common';
 
 import { storySettings } from './storySettings';
-import { CounterBadge } from '../../src';
-import ExampleCounterBadgesRaw from '!raw-loader!./ExampleCounterBadges';
-import ExampleCounterBadges from './ExampleCounterBadges';
+import { CounterBadge, Layout, Cell } from '../../src';
+import LiveCodeExample, {
+  createPropsArray,
+} from '../utils/Components/LiveCodeExample';
+
+const createCounterWithBadge = props => `
+<CounterBadge ${createPropsArray(props).join('\n       ')}>${
+  props.value
+}</CounterBadge>`;
+
+const counterBadgeSkinExample = `
+<div>
+  ${Object.keys(SKIN)
+    .map(skin => {
+      return `
+      <div style={{ 
+        display: 'flex',
+         padding: '5px 5px',
+        }}>${createCounterWithBadge({
+          skin,
+          value: 1,
+        })} - ${skin}
+      </div>`;
+    })
+    .join('\n       ')}
+</div>
+`;
+
+const exampleValues = [
+  { label: 'Single digit', value: 1 },
+  { label: '2 digits', value: 56 },
+  { label: 'Icon', value: '<Hint />' },
+];
+
+const iconValuesExample = `
+    <div>
+    ${exampleValues
+      .map(example => {
+        return `
+        <div style={{ 
+          display: 'flex',
+           padding: '5px 5px',
+          }}>${createCounterWithBadge({
+            value: example.value,
+          })} - ${example.label}
+        </div>`;
+      })
+      .join('\n       ')}
+    </div>
+`;
 
 export default {
   category: storySettings.category,
@@ -30,9 +76,22 @@ export default {
   hiddenProps: ['dataHook'],
   examples: (
     <div>
-      <CodeExample title="Variations" code={ExampleCounterBadgesRaw}>
-        <ExampleCounterBadges />
-      </CodeExample>
+      <Layout>
+        <Cell span={3}>
+          <LiveCodeExample
+            compact
+            title="Skins"
+            initialCode={counterBadgeSkinExample}
+          />
+        </Cell>
+        <Cell span={3}>
+          <LiveCodeExample
+            compact
+            title="Content"
+            initialCode={iconValuesExample}
+          />
+        </Cell>
+      </Layout>
     </div>
   ),
 };
